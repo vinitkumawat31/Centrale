6c1cdfbdb6fc4915cd5d18d95d50cf9e4af133da
vinitkumawat31 <vinitkumawat31@gmail.com>
2018-11-04 05:20:07 +0530

    almost final

--- app/app.py
+++ app/app.py
@@ -27,11 +27,21 @@ mysql = MySQL(app)
 @app.route('/',methods=['GET','POST'])
 
 def index():
+    if request.method== 'POST' and request.form['btn'] == 'search':
+        s = request.form
+        category = s['category']
+        if category == 'All':
+            return redirect('/')
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * from posts where category='" + category + "'")
+        posts = cur.fetchall()
+        return render_template('index.html', posts = posts, category = category)
     if request.method== 'POST' and request.form['btn'] == 'sign-up':     #get form data
         userDetails = request.form
         name = userDetails['name']
         email = userDetails['email']
         username = userDetails['username']
+        phone_number = userDetails['phone']
 
         cur = mysql.connection.cursor()
         cur.execute("SELECT * from users where username='" + username + "'")
@@ -40,15 +50,15 @@ def index():
             return 'username already exists'
         password = userDetails['password']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name,email,username,password) VALUES(%s,%s,%s,%s)",(name,email,username,password))
+        cur.execute("INSERT INTO users(name,email,username,password,phone_number) VALUES(%s,%s,%s,%s,%s)",(name,email,username,password,phone_number))
         mysql.connection.commit()
         cur.close()
-        return redirect('/users')
+        return redirect('/')
     cur = mysql.connection.cursor()
     resultValue = cur.execute("SELECT * FROM posts")
     if resultValue > 0:
         posts = cur.fetchall()
-        return render_template('index.html', posts = posts)
+        return render_template('index.html', posts = posts, category = None)
     return render_template('index.html')
 
 
@@ -88,22 +98,27 @@ def login():
 def profile():
     
     if 'user' in session:
+        username = session['user']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM notifications where username = '"+username+"' ORDER BY timestamp DESC LIMIT 3")
+        notify = cur.fetchall()
+        cur.close()
         cur = mysql.connection.cursor()
         cur1 = mysql.connection.cursor()
-        username = session['user']
+        
         resultValue = cur.execute("SELECT * FROM posts where username = '" + username +"'")
         resultValue1 = cur1.execute("SELECT * FROM sharing where username = '" + username +"'")
         if resultValue>0:
             Posts = cur.fetchall()
             if resultValue1>0:
                 Cabs = cur1.fetchall()
-                return render_template('profile.html',Posts = Posts, Cabs = Cabs)
-            return render_template('profile.html',Posts = Posts, Cabs = None)
+                return render_template('profile.html',Posts = Posts, Cabs = Cabs,notify=notify)
+            return render_template('profile.html',Posts = Posts, Cabs = None,notify=notify)
         else :
             if resultValue1>0:
                 Cabs = cur1.fetchall()
-                return render_template('profile.html',Posts = None, Cabs = Cabs)
-            return render_template('profile.html', Posts = None, Cabs = None)
+                return render_template('profile.html',Posts = None, Cabs = Cabs,notify=notify)
+            return render_template('profile.html', Posts = None, Cabs = None,notify=notify)
     return redirect('/login') 
     
 
@@ -127,17 +142,17 @@ def post_form():
              title = postDetails['title']
              description = postDetails['description']
              price = postDetails['price']
+             category = postDetails['category']
              filename = photos.save(request.files['image'])
              image_path = 'uploadedimages/' + filename
-             #image_path = postDetails['image']
-             #file.save(os.path.join(app.config['UPLOAD_FOLDER'], image_path))
+
              cur = mysql.connection.cursor()
              cur.execute("SELECT * from users where username='" + username + "'")
              row = cur.fetchone()
              user_id = row[0]
              cur.close()
              cur = mysql.connection.cursor()
-             cur.execute("INSERT INTO posts(username,user_id,title,description,price,image_path) VALUES(%s,%s,%s,%s,%s,%s)",(username,user_id,title,description,price,image_path))
+             cur.execute("INSERT INTO posts(username,user_id,title,description,price,image_path,category) VALUES(%s,%s,%s,%s,%s,%s,%s)",(username,user_id,title,description,price,image_path,category))
              mysql.connection.commit()
              cur.close()
         
@@ -173,10 +188,10 @@ def delete_post(post_id):
 def post_details(post_id):
     if 'user' in session:
         cur = mysql.connection.cursor()
-        username = session['user']
+        username = session['user'] # user whose comment
         cur.execute("SELECT * FROM posts where id = '" + post_id +"'")
         post = cur.fetchone()
-        post_user = post[1]
+        post_user = post[1]   # user whose post 
         cur.close()
         cur = mysql.connection.cursor()
         cur.execute("SELECT * from users where username='" + post_user + "'")
@@ -196,7 +211,17 @@ def post_details(post_id):
              cur.execute("INSERT INTO comment_post(username,post_id,text,user_id) VALUES(%s,%s,%s,%s)",(username,post_id,comment,user_id))
              mysql.connection.commit()
              cur.close()
+
+             #notify
+             text = "Comment on Item " + post[3]+" by "+ username
+             link = "/post_details/"+post_id
+             cur = mysql.connection.cursor()
+             cur.execute("INSERT INTO notifications(username,text,link) VALUES(%s,%s,%s)",(post_user,text,link))
+             mysql.connection.commit()
+             cur.close()
+
              return redirect('/post_details/'+ post_id)
+
         cur = mysql.connection.cursor()
         resultValue = cur.execute("SELECT * FROM comment_post where post_id = '" + post_id +" '")
         if resultValue > 0:
@@ -211,6 +236,13 @@ def post_details(post_id):
 @app.route('/sharing_index',methods=['GET','POST'])
 
 def sharing_index():
+    if request.method== 'POST' and request.form['btn'] == 'search':
+        s = request.form
+        date = s['date']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * from sharing where date='" + date + "'")
+        sharing = cur.fetchall()
+        return render_template('sharing_index.html', sharing = sharing, date=date)
     cur = mysql.connection.cursor()
     resultValue = cur.execute("SELECT * FROM sharing")
     if resultValue > 0:
@@ -245,6 +277,14 @@ def cab_details(cab_id):
              cur.execute("INSERT INTO comment_cab(username,cab_id,text,user_id) VALUES(%s,%s,%s,%s)",(username,cab_id,comment,user_id))
              mysql.connection.commit()
              cur.close()
+             #notify
+             text = "Comment on Cab Sharing from " + cab[6]+"to"+cab[7]+" on "+ str(cab[3]) +" by "+ username
+             link = "/cab_details/"+cab_id
+             cur = mysql.connection.cursor()
+             cur.execute("INSERT INTO notifications(username,text,link) VALUES(%s,%s,%s)",(cab_user,text,link))
+             mysql.connection.commit()
+             cur.close()
+
              return redirect('/cab_details/'+ cab_id)
         cur = mysql.connection.cursor()
         resultValue = cur.execute("SELECT * FROM comment_cab where cab_id = '" + cab_id +" '")
@@ -327,6 +367,50 @@ def delete_cab(cab_id):
             return 'persmission denied'
     else:
         return redirect('/login')
-    return redirect('/profile')         
+    return redirect('/profile') 
+@app.route('/delete_comment_cab/<comment_id>',methods=['GET','POST'])
+def delete_comment_cab(comment_id):
+    if 'user' in session:
+        cur = mysql.connection.cursor()
+        username = session['user']
+        cur.execute("SELECT * FROM comment_cab where id = '" + comment_id +"'")
+        comment = cur.fetchone()
+        cur.close()
+        if username == comment[1]:
+            post_id = comment[2]
+            cur = mysql.connection.cursor()
+            cur.execute("DELETE  FROM comment_cab where id = '" + comment_id +"'")
+            mysql.connection.commit()
+            cur.close()
+            return redirect('/sharing_index')
+
+        else : 
+            return 'persmission denied'    
+    else:
+        return redirect('/login')  
+        
+@app.route('/profile/control', methods=['GET','POST'])
+def profile_control():
+    if 'user' in session:
+        username = session['user']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM notifications where username = '"+username+"' ORDER BY timestamp DESC LIMIT 3")
+        notify = cur.fetchall()
+        cur.execute("SELECT * FROM notifications where username = '"+username+"' ORDER BY timestamp ")
+        all_notify = cur.fetchall()
+        cur = mysql.connection.cursor()
+        
+        cur.execute("SELECT * FROM comment_post where username = '" + username +"'")
+        comment_post = cur.fetchall()
+        cur.close()
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM comment_cab where username = '" + username +"'")
+        comment_cab = cur.fetchall()
+        cur.close()
+        return render_template('profile_control.html', comment_cab = comment_cab, comment_post=comment_post,notify = notify,all_notify=all_notify)
+
+    else :
+        return redirect('/login')    
+
 if __name__ == '__main__':
     app.run(debug=True)
